1dec353fa6c76d4983d90315b3cd58ebaed2e0be
Darryl Skeard <darryl.skeard@hotmail.com>
2025-06-10 21:27:53 -0230

    Edit fields and icon buttons added

---
+++ Signals/Signals/Controls/IconButton.axaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Signals.Controls;
+
+public class IconButton : Button
+{
+    public static readonly StyledProperty<string> IconTextProperty = AvaloniaProperty.Register<IconButton, string>(
+        nameof(IconText));
+
+    public string IconText
+    {
+        get => GetValue(IconTextProperty);
+        set => SetValue(IconTextProperty, value);
+    }
+}
